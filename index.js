23b6193217e95e8bebc6c87aa26d7e56ae425556
Michael Davis <jrmdd1496@gmail.com>
2022-05-04 17:09:38 -0500

    commit tree b5c6340e436a60f530048050e1a37a2d395155e4

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let ans = false;
+  for (const num of array) {
+    const altNum = target - num;
+    const indexOfLast = array.length -1;
+    let i = array.indexOf(num) +1;
+    while (i <= indexOfLast) {
+      if (altNum == array[i]) {
+        ans = true;
+        return ans;
+      }
+      else {
+        i++;
+      }
+    }
+  }
+  if (ans == true) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+
+console.log(hasTargetSum([1,2,3,4,5,6,7,8,9,10], 14));
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
+For each element subract number from target to get number needed for summation.
+Compare that number to numbers in array.
+  loop through array
+  if alternatice number found return true
+  if alternatice number not found move to next number
+if out of number reutnr false
   Add your pseudocode here
 */
 
